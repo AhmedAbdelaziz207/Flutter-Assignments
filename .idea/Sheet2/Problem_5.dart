524f25d9c98039c7a277fd6d0f6dcbcec85aacb7
ahmed <ahmed.abdelaziz3080@gmail.com>
2024-05-07 15:27:11 +0300

    Problem 5 Finished

---
+++ .idea/Sheet2/Problem_5.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+main() {
+  print("Enter size of numbers : ");
+  int? size = int.parse(stdin.readLineSync()!);
+  int even = 0, odd = 0;
+  int? number;
+  for (var i = 0; i < size; i++) {
+    number = int.parse(stdin.readLineSync()!);
+
+    if (number % 2 == 0) {
+      even++;
+    } else {
+      odd++;
+    }
+  }
+  
+  print("Even : $even , Odd : $odd ");
+}
