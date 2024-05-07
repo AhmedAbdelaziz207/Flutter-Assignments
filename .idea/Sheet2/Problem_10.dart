4afb271990b9b63edce6ab2ea98ed96cee626861
ahmed <ahmed.abdelaziz3080@gmail.com>
2024-05-07 21:10:05 +0300

    Problem 10 Finished

--- .idea/Sheet2/Problem_10.dart
+++ .idea/Sheet2/Problem_10.dart
@@ -3,5 +3,6 @@ main(){
   Map<String, String> user_info = {"name" : "Ahmed Abdelaziz ","age":"21", "phone ": "01030"};
   user_info.keys.forEach((key) {
     print("$key : ${user_info[key]}");
+
   });
 }
