def5f6b165378ff942a02445d04d3840b647e9e2
ahmed <ahmed.abdelaziz3080@gmail.com>
2024-05-10 19:25:17 +0300

    Sheet2 Problem 1

--- .idea/Sheet3/Problem_1.dart
+++ .idea/Sheet3/Problem_1.dart
@@ -1,4 +1,15 @@
 import 'dart:io';
 main(){
+  String password = "ahmed@123";
+  while(true){
+    stdout.write("Enter Password  : ");
+    String ? inputPassword = stdin.readLineSync()!;
 
+    if(inputPassword.toLowerCase() == password.toLowerCase() ){
+      print("Accepted , Password is $inputPassword ");
+      break;
+    }else{
+      print("Wrong password , try again ");
+    }
+  }
 }
